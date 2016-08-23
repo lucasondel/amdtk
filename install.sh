47d64ab62d1f080b22bc97e08e8ad633f511a826
Ondel Lucas <iondel@fit.vutbr.cz>
2016-08-23 09:56:13 +0200

    Update the installation scripts. Removed OpenFst from the installation process as until now, AMDTK does not make use of it.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # We avoid symbolic links as it may confuses the installation process.
 amdtk_root=$(pwd -P)
 
-anaconda_path="$amdtk_root/extras/miniconda3"
+anaconda_path="$HOME/anaconda3"
 env_name="py35_amdtk"
 
 while getopts ":e:hp:" opt; do
@@ -19,17 +19,15 @@ while getopts ":e:hp:" opt; do
             anaconda_path=$OPTARG
             ;;
         h)
-            echo "usage: $0 [OPTIONS]"
-            echo ""
-            echo "Install the dependencies needed to run AMDTK and some"
-            echo "recipes."
-            echo ""
-            echo "Options:"
+            echo "usage: $0 [OPTIONS]                                         "
+            echo "                                                            "
+            echo "Install the dependencies needed to run AMDTK recipes.       "
+            echo "                                                            "
+            echo "Options:                                                    "
             echo "  -e NAME     Anaconda environment name. Default: $env_name."
-            echo "  -h          Show this message."
-            echo "  -p PATH     Path to an existing Anaconda distribution. If"
-            echo "              not provided a Miniconda distribution will be"
-            echo "              installed."
+            echo "  -h          Show this message.                            "
+            echo "  -p PATH     Path to an existing Anaconda distribution.    "
+            echo "              Default: $anaconda_path.                      "
             exit 0
             ;;
         \?) 
@@ -43,46 +41,8 @@ while getopts ":e:hp:" opt; do
     esac
 done
 
-# Directory where we will install the external tools and python (miniconda)
-mkdir -p $amdtk_root/extras
-
-if [ ! -d $anaconda_path ]; then
-    echo "Installing Miniconda 3"
-
-    # Check the architecture of the machine.
-    arch=`uname -m`
-    case $arch in 
-        x86_64)
-            miniconda_url="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-            ;;
-        x86)
-            miniconda_url="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh"
-            ;;
-        *)
-            echo "Unknown architecture. You will have to install Anaconda on your own."
-            exit 1
-            ;;
-    esac
-
-    # Download miniconda. 
-    install_script="extras/Miniconda3-latest-Linux-$arch.sh"
-    if [ ! -e $install_script ]; then
-        wget -P $amdtk_root/extras $miniconda_url
-    else
-        echo Miniconda installer is already downloaded.
-    fi
-
-    # Install Miniconda 3. AMDTK is developed using python 3. To avoid any 
-    # conflict with any existing python environment we install Miniconda and 
-    # create a specific environment for AMDTK. If you want to use your own 
-    # python distribution, skip this step and change the paths accordingly.
-    if [ ! -e $amdtk_root/extras/miniconda3 ]; then
-        chmod +x $install_script
-        ./$install_script -b -p $amdtk_root/extras/miniconda3
-    else
-        echo Miniconda is already installed.
-    fi
-fi
+# Directory where we will install the external tools.
+mkdir -p $amdtk_root/tools
 
 # Set the path to the correct python distribution for the the rest of the 
 # installation.
@@ -90,88 +50,55 @@ unset PYTHONPATH
 export PATH=$anaconda_path/bin:$PATH
 
 # We create a specific environment for AMDTK if it doesn't exists.
+echo -n "Installing anaconda environment \"$env_name\"... "
 if [ -z "`conda env list | grep $env_name`" ]; then
     conda env create --name $env_name python=3 -f $amdtk_root/py35_amdtk.yml
-fi
+fi || exit 1
+echo "done"
 
-# Activate the new environment to install other dependencies.
-source activate $env_name
+# Download and install sph2pipe. This is needed for features extraction.
+echo -n "Installing sph2pipe... "
+if [ ! -e $amdtk_root/tools/sph2pipe_v2.5.tar.gz ]; then    
+    wget -P $amdtk_root/tools "https://www.ldc.upenn.edu/sites/www.ldc.upenn.edu/files/ctools/sph2pipe_v2.5.tar.gz" --no-check-certificate
+fi || exit 1
 
-# Install sph2pipe. This is needed for features extraction.
-if [ ! -e $amdtk_root/extras/sph2pipe_v2.5.tar.gz ]; then    
-    wget -P $amdtk_root/extras "https://www.ldc.upenn.edu/sites/www.ldc.upenn.edu/files/ctools/sph2pipe_v2.5.tar.gz" --no-check-certificate
-else
-    echo sph2pipe already downloaded.
-fi
-if [ ! -e $amdtk_root/extras/sph2pipe_v2.5 ]; then
-    tar xvf $amdtk_root/extras/sph2pipe_v2.5.tar.gz
-    mv $amdtk_root/sph2pipe_v2.5 $amdtk_root/extras
-    cd $amdtk_root/extras/sph2pipe_v2.5
+if [ ! -e $amdtk_root/tools/sph2pipe_v2.5 ]; then
+    tar xvf $amdtk_root/tools/sph2pipe_v2.5.tar.gz
+    mv $amdtk_root/sph2pipe_v2.5 $amdtk_root/tools
+    cd $amdtk_root/tools/sph2pipe_v2.5
     gcc -o sph2pipe *.c -lm
     cd ../../
-fi
+fi || exit 1
+echo "done"
 
-# set prefix for installations
-prefix="$anaconda_path"/envs/"$env_name"
 
-# Install OpenFst 3.5.3. We have shipped OpenFst into AMDTK as we have 
-# changed OpenFst's configuration script to be compatible with python 3.
-
-python -c "import pywrapfst"
-is_anaconda_installed=$?
-if [ $is_anaconda_installed -ne 0 ]; then
-    rm -fr $amdtk_root/openfst-1.5.3
-    tar xvf $amdtk_root/openfst-1.5.3.tar.gz
-    cd $amdtk_root/openfst-1.5.3 
-    ./configure --enable-python --enable-far --prefix=$prefix
-    make 
-    make install
-    cd ../
-else
-    echo OpenFst already installed.
-fi
-
-# Compile and install fstphicompose
-#install_prefix="$amdtk_root"/extras/bin
-#if [[ ! -f "$install_prefix"/fstphicompose ]]; then
-#    mkdir -p "$install_prefix"
-#    cd "$amdtk_root"/tools/fstphicompose
-#    make PREFIX="$prefix"
-#    make install INSTALL_PREFIX="$install_prefix"
-#    cd ../../
-#fi
-
+echo -n "Creating path file... "
+new_path="$amdtk_root/scripts:$amdtk_root/tools/sph2pipe_v2.5:\$PATH"
 # Create the path.sh file to use the newly created environment.
-cp $amdtk_root/path_template.sh $amdtk_root/extras/path.sh
-echo >> $amdtk_root/extras/path.sh
-echo "# Setting python environment." >> $amdtk_root/extras/path.sh
-echo "unset PYTHONPATH" >> $amdtk_root/extras/path.sh
-echo "export PYTHONPATH=$amdtk_root" >> $amdtk_root/extras/path.sh
-echo "export PATH=$amdtk_root/scripts:$amdtk_root/extras/sph2pipe_v2.5:$amdtk_root/extras/bin:$anaconda_path/bin:\$PATH" >> $amdtk_root/extras/path.sh
-echo >> $amdtk_root/extras/path.sh
-echo "# Adding the tools directory." >> $amdtk_root/extras/path.sh
-echo "export PATH=$amdtk_root/tools:\$PATH" >> $amdtk_root/extras/path.sh
-echo "" >> $amdtk_root/extras/path.sh
-echo "# Selecting the AMDTK environment." >> $amdtk_root/extras/path.sh
-echo "export PATH=$anaconda_path/envs/$env_name/bin:\$PATH" >> $amdtk_root/extras/path.sh
-echo "" >> $amdtk_root/extras/path.sh
-echo "#Extending LD_LIBRARY_PATH" >> $amdtk_root/extras/path.sh
-echo "export LD_LIBRARY_PATH=$prefix/lib:\$LD_LIBRARY_PATH" >> $amdtk_root/extras/path.sh
-
-#echo "source activate $env_name" >> $amdtk_root/extras/path.sh
+cp "$amdtk_root/path_template.sh" "$amdtk_root/tools/path.sh" || exit 1
+echo "# Setting python environment.            " >> "$amdtk_root/tools/path.sh"
+echo "unset PYTHONPATH                         " >> "$amdtk_root/tools/path.sh"
+echo "export PYTHONPATH=$amdtk_root            " >> "$amdtk_root/tools/path.sh"
+echo "                                         " >> "$amdtk_root/tools/path.sh"
+echo "# Add extra tools to the PATH.           " >> "$amdtk_root/tools/path.sh"
+echo "export PATH=$new_path:$PATH              " >> "$amdtk_root/tools/path.sh"
+echo "                                         " >> "$amdtk_root/tools/path.sh"
+echo "# Selecting the AMDTK environment.       " >> "$amdtk_root/tools/path.sh"
+echo "source activate $env_name                " >> "$amdtk_root/tools/path.sh"
+echo "done"
 
 # Install the recipes.
-echo -n "Copying 'path.sh' file into recipes' directory... "
+echo -n "Installing recipes... "
 # timit recipe
-cp $amdtk_root/extras/path.sh $amdtk_root/recipes/timit
+cp "$amdtk_root/tools/path.sh" "$amdtk_root/recipes/timit" || exit 1
 # wsj recipe
-cp $amdtk_root/extras/path.sh $amdtk_root/recipes/wsj
-ln -s $amdtk_root/recipes/timit/utils $amdtk_root/recipes/wsj/utils
+cp "$amdtk_root/tools/path.sh" "$amdtk_root/recipes/wsj" || exit 1
+ln -fs "$amdtk_root/recipes/timit/utils" "$amdtk_root/recipes/wsj/utils" || exit 1
 # babel turkish recipe
-cp $amdtk_root/extras/path.sh $amdtk_root/recipes/babel_turkish_clsp
-ln -s $amdtk_root/recipes/timit/utils $amdtk_root/recipes/babel_turkish_clsp/utils
+cp "$amdtk_root/tools/path.sh" "$amdtk_root/recipes/babel_turkish_clsp" || exit 1
+ln -fs "$amdtk_root/recipes/timit/utils" "$amdtk_root/recipes/babel_turkish_clsp/utils" || exit 1
 # WSJ no punctuation recipe
-cp $amdtk_root/extras/path.sh $amdtk_root/recipes/wsj_no_punc
-ln -s $amdtk_root/recipes/timit/utils $amdtk_root/recipes/wsj_no_punc/utils
+cp "$amdtk_root/tools/path.sh" "$amdtk_root/recipes/wsj_no_punc" || exit 1
+ln -fs "$amdtk_root/recipes/timit/utils $amdtk_root/recipes/wsj_no_punc/utils" || exit 1
 echo done
 
