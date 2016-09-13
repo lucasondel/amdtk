bf495e86fe08c3bb14cf59d937e7fd5d01f1d1d1
Lucas Ondel <iondel@fit.vutbr.cz>
2016-09-13 11:16:52 +0200

    Minor changes.

--- tests/test_discrete_latent_model.py
+++ tests/test_discrete_latent_model.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 from amdtk.models import DiscreteLatentModel
 from amdtk.models import DiscreteLatentModelEmptyListError
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import unittest
 import numpy as np
