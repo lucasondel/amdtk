8fccc50636d702550f3546200e5cb954c88e399f
Ondel Lucas <iondel@fit.vutbr.cz>
2016-12-01 17:13:25 +0100

    pep8 compliance.

--- amdtk/models/normal_gamma.py
+++ amdtk/models/normal_gamma.py
@@ -1,43 +1,21 @@
 
-""" Normal-Gamma density."""
+""" Normal-Gamma prior for a Gaussian with diagonal covariance."""
 
 import numpy as np
 from scipy.special import gammaln, psi
 
 
 class NormalGamma(object):
-    """Normal-Gamma density.
+    """NormalGamma density."""
 
-    Attributes
-    ----------
-    mu : numpy.ndarray
-        Mean of the Gaussian density.
-    kappa : float
-        Factor of the precision matrix.
-    alpha : float
-        Shape parameter of the Gamma density.
-    beta : numpy.ndarray
-        Rate parameters of the Gamma density.
 
-    Methods
-    -------
-    expLogPrecision()
-        Expected value of the logarithm of the precision.
-    expPrecision()
-        Expected value of the precision.
-    KL(self, pdf)
-        KL divergence between the current and the given densities.
-    newPosterior(self, stats)
-        Create a new Normal-Gamma density.
-    """
-
-    def __init__(self, mu, kappa, alpha, beta):
-        self.mu = mu
+    def __init__(self, mean, kappa, alpha, beta):
+        self.mean = mean
         self.kappa = kappa
         self.alpha = alpha
         self.beta = beta
 
-    def expLogPrecision(self):
+    def expected_log_precision(self):
         '''Expected value of the logarithm of the precision.
 
         Returns
@@ -47,7 +25,7 @@ class NormalGamma(object):
         '''
         return psi(self.alpha) - np.log(self.beta)
 
-    def expPrecision(self):
+    def expected_precision(self):
         """Expected value of the precision.
 
         Returns
@@ -57,8 +35,13 @@ class NormalGamma(object):
         """
         return self.alpha/self.beta
 
-    def KL(self, q):
+    def kl_divergence(self, prob):
         """KL divergence between the current and the given densities.
+
+        Parameters
+        ----------
+        prob : :class:`NormalGamma`
+            NormalGamma density to compute the divergence with.
 
         Returns
         -------
@@ -66,22 +49,23 @@ class NormalGamma(object):
             KL divergence.
 
         """
-        p = self
+        exp_lambda = self.expected_precision()
+        exp_log_lambda = self.expected_log_precision()
 
-        exp_lambda = p.expPrecision()
-        exp_log_lambda = p.expLogPrecision()
+        kl_div = .5 * (np.log(self.kappa) - np.log(prob.kappa))
+        kl_div -= .5 * (1 - prob.kappa * \
+            (1./self.kappa + exp_lambda * (self.mean - prob.mean)**2))
+        kl_div += gammaln(prob.alpha) - gammaln(self.alpha)
+        kl_div += self.alpha * np.log(self.beta) - prob.alpha * \
+            np.log(prob.beta)
+        kl_div += exp_log_lambda * (self.alpha - prob.alpha)
+        kl_div -= exp_lambda * (self.beta - prob.beta)
 
-        return (.5 * (np.log(p.kappa) - np.log(q.kappa))
-                - .5 * (1 - q.kappa * (1./p.kappa + exp_lambda * (p.mu - q.mu)**2))
-                - (gammaln(p.alpha) - gammaln(q.alpha))
-                + (p.alpha * np.log(p.beta) - q.alpha * np.log(q.beta))
-                + exp_log_lambda * (p.alpha - q.alpha)
-                - exp_lambda * (p.beta - q.beta)).sum()
+        return kl_div.sum()
 
-    def newPosterior(self, stats):
-        """Create a new Normal-Gamma density.
 
-        Create a new Normal-Gamma density given the parameters of the
+    def new_posterior(self, stats):
+        """Create a new Normal-Gamma density given the parameters of the
         current model and the statistics provided.
 
         Parameters
@@ -95,14 +79,13 @@ class NormalGamma(object):
             New Dirichlet density.
 
         """
-        # stats[0]: counts
-        # stats[1]: sum(x)
-        # stats[2]: sum(x**2)
         kappa_n = self.kappa + stats[0]
-        mu_n = (self.kappa * self.mu + stats[1]) / kappa_n
+        mean_n = (self.kappa * self.mean + stats[1]) / kappa_n
         alpha_n = self.alpha + .5 * stats[0]
-        v = (self.kappa * self.mu + stats[1])**2
-        v /= (self.kappa + stats[0])
-        beta_n = self.beta + 0.5*(-v + stats[2] + self.kappa * self.mu**2)
+        value = (self.kappa * self.mean + stats[1])**2
+        value /= (self.kappa + stats[0])
+        beta_n = self.beta + 0.5*(-value + stats[2] + self.kappa * \
+            self.mean**2)
 
-        return NormalGamma(mu_n, kappa_n, alpha_n, beta_n)
+        return NormalGamma(mean_n, kappa_n, alpha_n, beta_n)
+
