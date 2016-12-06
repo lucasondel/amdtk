f77445c6d321fe98ddfbce4df8ae9c09cbb6859d
Ondel Lucas <iondel@fit.vutbr.cz>
2016-12-06 13:33:53 +0100

    Added installation setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='amdtk',
+      version='1.0.0',
+      description='Acoustic Model Discovery Toolkit',
+      author='Lucas Ondel',
+      author_email='lucas.ondel@gmail.com',
+      packages=['amdtk'],
+      license='BSD')
