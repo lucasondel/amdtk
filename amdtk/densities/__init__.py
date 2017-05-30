6b81fa28bbce9ecaa331d378540c36831ff4e48e
Lucas Ondel <lucas.ondel@gmail.com>
2017-05-30 15:53:10 -0400

    Refactoring mixture model.

---
+++ amdtk/densities/__init__.py
@@ -0,0 +1,11 @@
+
+"""Acoustic Model Discovery Toolkit (AMDTK) module.
+Set of tools to do Bayesian clustering of raw acoustic
+features to automatically discover phone-like units.
+
+"""
+
+from .dirichlet import Dirichlet
+from .normal_gamma import NormalGamma
+from .normal import NormalDiag
+
---
+++ amdtk/training/__init__.py
@@ -0,0 +1,9 @@
+
+"""Acoustic Model Discovery Toolkit (AMDTK) module.
+Set of tools to do Bayesian clustering of raw acoustic
+features to automatically discover phone-like units.
+
+"""
+
+from .inference import StochasticVBOptimizer
+
